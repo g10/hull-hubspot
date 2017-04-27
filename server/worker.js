6870ede1874bb1dab4809712489abfc92655be82
mkawalek <michalkawalekdevkit@icloud.com>
2017-04-27 10:42:22 +0200

    appMiddleware added to worker

--- server/worker.js
+++ server/worker.js
@@ -1,9 +1,12 @@
+import AppMiddleware from "./lib/middleware/app";
+
 module.exports = function worker(options = {}) {
   const { connector, jobs } = options;
 
   connector.worker({
     jobs
-  });
+  })
+    .use(AppMiddleware());
 
   connector.startWorker();
 };
