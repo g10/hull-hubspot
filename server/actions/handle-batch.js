7eb66fefbb0912f7b9671443979d14dc6215cfd3
Michal Raczka <michal@raczka.me>
2017-06-09 17:04:31 +0200

    Upgrade hull-node and small fixes to batch endpointg

--- server/actions/handle-batch.js
+++ server/actions/handle-batch.js
@@ -1,12 +1,21 @@
 import _ from "lodash";
+import Promise from "bluebird";
 
 export default function handleBatch(ctx, messages) {
-  const users = messages.map(m => m.user);
-  ctx.metric.event({
-    title: "batch",
-    text: `User count: ${users.length}`
+  const users = messages.map(m => {
+    const segmentIds = m.segments.map(s => s.id);
+    m.user.segment_ids = (m.user.segment_ids || []).concat(segmentIds);
+    return m.user;
   });
   const filteredUsers = users.filter(user => ctx.shipApp.syncAgent.userWhitelisted(user) && !_.isEmpty(user.email));
+  ctx.client.logger.debug("outgoing.users.batch", {
+    preFilter: users.length,
+    postFilter: filteredUsers.length
+  });
+
+  if (filteredUsers.length === 0) {
+    return Promise.resolve("ok");
+  }
   return ctx.enqueue("sendUsersJob", {
     users: filteredUsers
   });
