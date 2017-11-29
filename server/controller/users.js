809bc9adabde7d73ec95f4475d3c4191cbfd49fc
Michal Raczka <michal@raczka.me>
2017-11-29 10:25:13 +0100

    Fix outgoing logs

--- server/controller/users.js
+++ server/controller/users.js
@@ -30,7 +30,6 @@ export default class UsersController {
       .then(({ hubspotProperties }) => {
         const body = users.map((user) => {
           const properties = ctx.shipApp.syncAgent.mapping.getHubspotProperties(ctx.segments, hubspotProperties, user);
-          ctx.client.logger.debug("outgoing.user", { email: user.email, properties });
           return {
             email: user.email,
             properties
@@ -44,7 +43,10 @@ export default class UsersController {
           }
 
           if (res.statusCode === 202) {
-            users.map(u => ctx.client.asUser(_.pick(u, ["email", "id", "external_id"])).logger.info("outgoing.user.success"));
+            users.map(hullUser => {
+              const user = _.find(body, { email: hullUser.email });
+              return ctx.client.asUser(hullUser).logger.info("outgoing.user.success", user.properties);
+            });
             return Promise.resolve();
           }
           return Promise.reject(new Error("Error in create/update batch"));
@@ -75,7 +77,10 @@ export default class UsersController {
               return ctx.shipApp.hubspotAgent.batchUsers(retryBody)
                 .then(res => {
                   if (res.statusCode === 202) {
-                    retryBody.map(u => ctx.client.asUser(_.pick(u, ["email", "id", "external_id"])).logger.info("outgoing.user.success"));
+                    retryBody.map((hullUser) => {
+                      const user = _.find(retryBody, { email: hullUser.email });
+                      return ctx.client.asUser(hullUser).logger.info("outgoing.user.success", user.properties);
+                    });
                     return Promise.resolve("ok");
                   }
                   return Promise.reject(new Error("Error in create/update batch"));
